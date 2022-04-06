50e93934d75a1049107e7a6732f4dd7418d3d7a9
lina-conti <linavarellaconti@gmail.com>
2022-04-06 15:58:39 +0200

    added euclidian distances

--- proj.py
+++ proj.py
@@ -14,6 +14,12 @@ from matplotlib import pyplot as plt
 def cosine(veca, vecb):
     return (np.dot(veca, vecb)/(np.linalg.norm(veca)*np.linalg.norm(vecb)))
 
+def squared_euclidian_distance(veca, vecb):
+    res = 0
+    for i in range(0, len(veca)):
+        res = res + (veca[i] - vecb[i]) * (veca[i] - vecb[i])
+    return res
+
 
 
 # creates a dictionary ([str -> map object]) that maps a word to its vector representation
@@ -85,6 +91,24 @@ def get_compare_pairs(sample1:dict, sample2:dict, num_comparisons):
 
     return words, edit_distance, cosine_dists
 
+def get_compare_pairs_ed(sample1:dict, sample2:dict, num_comparisons):
+    '''get the edit distance and euclidian distances in both datasets for a specified
+    number of words to be compared'''
+    words = []
+    edit_distance = []
+    eucl_dists = []
+
+    for i in range(0, num_comparisons):
+        w1 = random.sample(sample1.keys(), 1)[0] #random.sample returns a list of length 1, we take the item
+        w2 = random.sample(sample2.keys(), 1)[0]
+        if w1 != w2:
+            words.append((w1, w2))
+            edit_distance.append(editdistance.distance(w1, w2))
+            eucl_dists.append(squared_euclidian_distance(sample1[w1], sample2[w2]))
+    edit_distance = np.array(edit_distance)[:, np.newaxis]
+
+    return words, edit_distance, eucl_dists
+
 def full_compare(sample: dict):
     '''runs a comprehensive comparison between every unique pair of words in a sample.
     returns the edit distances and cosine similarities'''
@@ -100,22 +124,50 @@ def full_compare(sample: dict):
     return(e_dists, cosine_dists)
 
 # ------------------------------- WRITING OUR SAMPLES TO A FILE -----------------------------------------------------
-'''
+"""
 top_vectors_sw = load_vectors("wiki-news-300d-1M-subword.vec", 0.1)
+top_vectors_no_sw = load_vectors("wiki-news-300d-1M.vec", 0.1)
 sample_1_sw, sample_1_no_sw = sample_words(top_vectors_sw, top_vectors_no_sw, 1000, filter)
 sample_2_sw, sample_2_no_sw = sample_words(top_vectors_sw, top_vectors_no_sw, 1000, filter)
 
 # writes the samples to files to keep them for future use
-write_to_file("morpho2022/sample_1_sw.vec", sample_1_sw)
-write_to_file("morpho2022/sample_1_no_sw.vec", sample_1_no_sw)
-write_to_file("morpho2022/sample_2_sw.vec", sample_2_sw)
-write_to_file("morpho2022/sample_2_no_sw.vec", sample_2_no_sw)
-'''
+write_to_file("morpho_project/morpho2022/sample_1_sw.vec", sample_1_sw)
+write_to_file("morpho_project/morpho2022/sample_1_no_sw.vec", sample_1_no_sw)
+write_to_file("morpho_project/morpho2022/sample_2_sw.vec", sample_2_sw)
+write_to_file("morpho_project/morpho2022/sample_2_no_sw.vec", sample_2_no_sw)
+"""
+
+# ------------------------------- R^2 SCORES EUCLIDIAN DISTANCES -----------------------------------------------------
+''' Now using new random compare  '''
+
+#load the samples
+sw_sample_1 = load_vectors('morpho_project/morpho2022/sample_1_sw.vec', 1)
+sw_sample_2 = load_vectors('morpho_project/morpho2022/sample_2_sw.vec', 1)
+
+no_sw_sample_1 = load_vectors('morpho_project/morpho2022/sample_1_no_sw.vec', 1)
+no_sw_sample_2 = load_vectors('morpho_project/morpho2022/sample_2_no_sw.vec', 1)
+
+#make comparisons
+sw_words, sw_edists, sw_eucld = get_compare_pairs_ed(sw_sample_1, sw_sample_2, 10000)
+no_sw_words, no_sw_edists, no_sw_eucld = get_compare_pairs_ed(no_sw_sample_1, no_sw_sample_2, 10000)
+
+
+r_with = linear_model.LinearRegression()
+r_without = linear_model.LinearRegression()
+
+
+X_train_sw, X_test_sw, y_train_sw, y_test_sw = train_test_split(sw_edists, sw_eucld, test_size=0.2, random_state=42)
+X_train_no_sw, X_test_no_sw, y_train_no_sw, y_test_no_sw = train_test_split(no_sw_edists, no_sw_eucld, test_size=0.2, random_state=42)
+
+r_with.fit(X_train_sw, y_train_sw)
+r_without.fit(X_train_no_sw, y_train_no_sw)
+
+print(f"R^2 with subword: {r_with.score(X_test_sw, y_test_sw)}")
+print(f"R^2 without subword: {r_with.score(X_test_no_sw, y_test_no_sw)}")
 
 # ------------------------------- R^2 SCORES MAIN -----------------------------------------------------
 ''' Now using new random compare  '''
-
-'''
+"""
 #load the samples
 sw_sample_1 = load_vectors('morpho_project/morpho2022/sample_1_sw.vec', 1)
 sw_sample_2 = load_vectors('morpho_project/morpho2022/sample_2_sw.vec', 1)
@@ -140,10 +192,9 @@ r_without.fit(X_train_no_sw, y_train_no_sw)
 
 print(f"R^2 with subword: {r_with.score(X_test_sw, y_test_sw)}")
 print(f"R^2 without subword: {r_with.score(X_test_no_sw, y_test_no_sw)}")
-
-
+"""
 # ------------------------------- HISTOGRAMS -----------------------------------------------------
-
+"""
 plt.hist(sw_edists)
 plt.title('Distribution of edit distances for 800 word pairs from a sample of 1000 words')
 plt.xlabel('edit distance')
@@ -163,10 +214,10 @@ plt.title('Full distribution of subword information for a 500 word sample, no su
 plt.xlabel('cosine similarity')
 plt.ylabel('number of word pairs')
 plt.show()
-
+"""
 # ------------------------------- PLOT MAIN -----------------------------------------------------
 
-
+"""
 #plot actual values for each set
 plt.scatter(sw_edists, sw_cosines, color = 'red', label = 'with subwords')
 plt.scatter(no_sw_edists, no_sw_cosines, color = 'blue', label = 'without subwords')
@@ -186,5 +237,4 @@ plt.xlabel('edit distance')
 plt.ylabel('cosine similarity')
 plt.legend()
 plt.show()
-'''
-
+"""
