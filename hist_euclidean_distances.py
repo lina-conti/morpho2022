898e76347b7c65f9852e9efb87a868a85d197378
lina-conti <linavarellaconti@gmail.com>
2022-04-07 14:29:18 +0200

    Euclidean histograms and plots

--- hist_euclidean_distances.py
+++ hist_euclidean_distances.py
@@ -1,22 +1,37 @@
-# ------------------------------- HISTOGRAMS -----------------------------------------------------
-"""
-plt.hist(sw_edists)
-plt.title('Distribution of edit distances for 800 word pairs from a sample of 1000 words')
-plt.xlabel('edit distance')
-plt.ylabel('number of word pairs')
-plt.show()
+import functions
+from functions import load_vectors, get_compare_pairs_ed
+import numpy as np
+import io
+import re
+import random
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+from matplotlib import pyplot as plt
 
-#plt.hist(cos_with, bins = 50) #results of full_compare
-plt.hist(sw_cosines, bins = 50) #results of random compare
-plt.title('Full distribution of cosine similarities for a 500 word sample, subword information included')
-plt.xlabel('cosine similarity')
+# ------------------------------- HISTOGRAMS -----------------------------------------------------
+
+#load the samples
+sw_sample_1 = load_vectors('morpho_project/morpho2022/sample_1_sw.vec', 1)
+sw_sample_2 = load_vectors('morpho_project/morpho2022/sample_2_sw.vec', 1)
+
+no_sw_sample_1 = load_vectors('morpho_project/morpho2022/sample_1_no_sw.vec', 1)
+no_sw_sample_2 = load_vectors('morpho_project/morpho2022/sample_2_no_sw.vec', 1)
+
+#make comparisons
+sw_words, sw_edists, sw_eucld = get_compare_pairs_ed(sw_sample_1, sw_sample_2, 10000)
+no_sw_words, no_sw_edists, no_sw_eucld = get_compare_pairs_ed(no_sw_sample_1, no_sw_sample_2, 10000)
+
+
+plt.hist(sw_eucld, bins = 50, label="with subword information") #results of random compare
+plt.title('Full distribution of euclidean distances for 10000 pairs of vectors including subword information')
+plt.xlabel('euclidean distance')
 plt.ylabel('number of word pairs')
 #plt.show()
 
-#plt.hist(cos_without, bins = 50) #full compare
-plt.hist(no_sw_cosines, bins = 50, color = 'red') #random compare
-plt.title('Full distribution of subword information for a 500 word sample, no subword information')
-plt.xlabel('cosine similarity')
+
+plt.hist(no_sw_eucld, bins = 50, color = 'red', label= "without subword information") #random compare
+plt.title('Full distribution of euclidean distances for 10,000 word pairs')
+plt.xlabel('euclidean distance')
 plt.ylabel('number of word pairs')
+plt.legend()
 plt.show()
-"""
