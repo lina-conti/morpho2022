435ec6f328d7cd279748ac1cef8d8837f6bfba5e
lina-conti <linavarellaconti@gmail.com>
2022-04-21 11:27:41 +0200

    Calculate and show results for morphologically related pairs

--- morphologically_related.py
+++ morphologically_related.py
@@ -1,18 +1,63 @@
-import functions
 from functions import *
-from sklearn import linear_model
-from sklearn.model_selection import train_test_split
-from matplotlib import pyplot as plt
-
+import scipy.stats as sp
 
 
 
 word_pairs, edit_d, cosine_s_sw, euclid_d_sw = words_to_features("morpho_project/morpho2022/pairs_sw.vec", "morpho_project/morpho2022/sampled_pairs")
 word_pairs, edit_d, cosine_s_no_sw, euclid_d_no_sw = words_to_features("morpho_project/morpho2022/pairs_no_sw.vec", "morpho_project/morpho2022/sampled_pairs")
 
-print(word_pairs)
-print(len(edit_d))
-print(len(cosine_s_sw))
-print(len(cosine_s_no_sw))
-print(len(euclid_d_sw))
-print(len(euclid_d_no_sw))
+sp_cosine_with = sp.linregress(edit_d[:10000], cosine_s_sw[:10000])
+sp_cosine_without = sp.linregress(edit_d[:10000], cosine_s_no_sw[:10000])
+
+sp_euclid_with = sp.linregress(edit_d[:10000], euclid_d_sw[:10000])
+sp_euclid_without = sp.linregress(edit_d[:10000], euclid_d_no_sw[:10000])
+
+# ------------------------------- SLOPE, R2 AND PVALUE  -----------------------------------------------------
+
+print('\nCOSINE SIMILARITY')
+print('\nwith subword')
+print(f'slope: {sp_cosine_with.slope}\n\
+r2 score: {sp_cosine_with.rvalue}\npval: {sp_cosine_with.pvalue}')
+print('\nwithout subword')
+print(f'slope: {sp_cosine_without.slope}\n\
+r2 score: {sp_cosine_without.rvalue}\n\
+pval: {sp_cosine_without.pvalue}')
+
+print('\n\nEUCLIDIAN DISTANCE')
+print('\nwith subword')
+print(f'slope: {sp_euclid_with.slope}\n\
+r2 score: {sp_euclid_with.rvalue}\npval: {sp_euclid_with.pvalue}')
+print('\nwithout subword')
+print(f'slope: {sp_euclid_without.slope}\n\
+r2 score: {sp_euclid_without.rvalue}\n\
+pval: {sp_euclid_without.pvalue}')
+
+# ------------------------------- PLOTTING COSINE  -----------------------------------------------------
+
+plt.plot(edit_d[:10000], sp_cosine_with.intercept + sp_cosine_with.slope*np.array(edit_d[:10000]), 'r', label = 'with subwords')
+plt.plot(edit_d[:10000], sp_cosine_without.intercept + sp_cosine_without.slope*np.array(edit_d[:10000]),'b', label = 'without subwords')
+
+#plot actual values for each set
+plt.scatter(edit_d[:10000], cosine_s_sw[:10000], color = 'red')
+plt.scatter(edit_d[:10000], cosine_s_no_sw[:10000], color = 'blue')
+
+plt.title('cosine similarity predicted from edit distance \n sample size 10000 word pairs')
+plt.xlabel('edit distance')
+plt.ylabel('cosine similarity')
+plt.legend()
+plt.show()
+
+# ------------------------------- PLOTTING EUCLIDEAN  -----------------------------------------------------
+
+plt.plot(edit_d[:10000], sp_euclid_with.intercept + sp_euclid_with.slope*np.array(edit_d[:10000]), 'r', label = 'with subwords')
+plt.plot(edit_d[:10000], sp_euclid_without.intercept + sp_euclid_without.slope*np.array(edit_d[:10000]),'b', label = 'without subwords')
+
+#plot actual values for each set
+plt.scatter(edit_d[:10000], euclid_d_sw[:10000], color = 'red')
+plt.scatter(edit_d[:10000], euclid_d_no_sw[:10000], color = 'blue')
+
+plt.title('euclidean distance predicted from edit distance \n sample size 10000 word pairs')
+plt.xlabel('edit distance')
+plt.ylabel('euclidean distance')
+plt.legend()
+plt.show()
