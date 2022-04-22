9dbe25ca8a40c9e32341b94364777dec0fde7205
lina-conti <linavarellaconti@gmail.com>
2022-04-22 14:59:56 +0200

    Separated scipy from sklearn in r2_euclidean_distances.py

--- r2_euclidean_distances.py
+++ r2_euclidean_distances.py
@@ -9,9 +9,6 @@ from sklearn.model_selection import train_test_split
 from matplotlib import pyplot as plt
 import scipy.stats as sp
 
-
-# ------------------------------- R^2 SCORES EUCLIDIAN DISTANCES -----------------------------------------------------
-
 #load the samples
 sw_sample_1 = load_vectors('sample_1_sw.vec', 1)
 sw_sample_2 = load_vectors('sample_2_sw.vec', 1)
@@ -19,17 +16,14 @@ sw_sample_2 = load_vectors('sample_2_sw.vec', 1)
 no_sw_sample_1 = load_vectors('sample_1_no_sw.vec', 1)
 no_sw_sample_2 = load_vectors('sample_2_no_sw.vec', 1)
 
+# ------------------------------- WITH SKLEARN -----------------------------------------------------
+
 #make comparisons for sklearn
 sw_words, sw_edists, sw_eucld = get_compare_pairs_ed(sw_sample_1, sw_sample_2, 10000)
 no_sw_words, no_sw_edists, no_sw_eucld = get_compare_pairs_ed(no_sw_sample_1, no_sw_sample_2, 10000)
 
-#comparisons for scipy
-sp_sw_words, sp_sw_edists, sp_sw_eucld = get_compare_pairs_ed(sw_sample_1, sw_sample_2, 10000, return_nparray = False)
-sp_no_sw_words, sp_no_sw_edists, sp_no_sw_eucld = get_compare_pairs_ed(no_sw_sample_1, no_sw_sample_2, 10000, return_nparray=False)
-
 r_with = linear_model.LinearRegression()
 r_without = linear_model.LinearRegression()
-
 
 X_train_sw, X_test_sw, y_train_sw, y_test_sw = train_test_split(sw_edists, sw_eucld, test_size=0.2, random_state=42)
 X_train_no_sw, X_test_no_sw, y_train_no_sw, y_test_no_sw = train_test_split(no_sw_edists, no_sw_eucld, test_size=0.2, random_state=42)
@@ -37,24 +31,34 @@ X_train_no_sw, X_test_no_sw, y_train_no_sw, y_test_no_sw = train_test_split(no_s
 r_with.fit(X_train_sw, y_train_sw)
 r_without.fit(X_train_no_sw, y_train_no_sw)
 
+print("SKLEARN\n")
 print(f"R^2 with subword: {r_with.score(X_test_sw, y_test_sw)}")
 print(f"R^2 without subword: {r_with.score(X_test_no_sw, y_test_no_sw)}")
 
+# ------------------------------- WITH SCIPY -----------------------------------------------------
 
-print(len(sw_edists), len(sw_eucld))
+#comparisons for scipy
+sp_sw_words, sp_sw_edists, sp_sw_eucld = get_compare_pairs_ed(sw_sample_1, sw_sample_2, 10000, return_nparray = False)
+sp_no_sw_words, sp_no_sw_edists, sp_no_sw_eucld = get_compare_pairs_ed(no_sw_sample_1, no_sw_sample_2, 10000, return_nparray=False)
+
 sp_with = sp.linregress(sp_sw_edists, sp_sw_eucld)
 sp_without = sp.linregress(sp_no_sw_edists, sp_no_sw_eucld)
 
+print("\nSCIPY")
+print('\nwith subword')
 print(f'slope: {sp_with.slope}\n\
-     r2 score for subword model: {sp_with.rvalue}\npval for subword model: {sp_with.pvalue}')
-
+r2 score: {sp_with.rvalue}\
+\npval: {sp_with.pvalue}')
+print('\nwithout subword')
 print(f'slope: {sp_without.slope}\n\
-     r2 score for no subword model: {sp_without.rvalue}\n\
-         pval for no subword model: {sp_without.pvalue}')
+r2 score: {sp_without.rvalue}\n\
+pval: {sp_without.pvalue}')
+
+# ------------------------------- COMPARISON PLOT -----------------------------------------------------
 
 plt.plot(sp_sw_edists, sp_with.intercept + sp_with.slope*np.array(sp_sw_edists), 'r', label = 'euclidean distance with subwords')
 plt.plot(sp_no_sw_edists, sp_without.intercept + sp_without.slope*np.array(sp_no_sw_edists),'b', label = 'euclidean distance without subwords')
 plt.plot(sw_edists, r_with.predict(sw_edists), color = 'pink', label = 'sklearn with subword informtation')
-plt.plot(no_sw_edists, r_without.predict(no_sw_edists), color = 'teal', label = 'sklearn without subword informtation')
+plt.plot(no_sw_edists, r_without.predict(no_sw_edists), color = 'teal', label = 'sklearn without subword information')
 plt.legend()
 plt.show()
